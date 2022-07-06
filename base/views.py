7615634120070b2431b689a4856b2d8c99dae984
Luis_miguel123 <luis.m@escolar.ifrn.edu.br>
2022-07-06 16:14:42 -0300

    novas urls
    new urls

--- base/views.py
+++ base/views.py
@@ -8,3 +8,24 @@ class homeView(TemplateView):
 
 class loginView(TemplateView):
     template_name= "login.html"
+    
+class filmesView(TemplateView):
+    template_name= "filmes.html"
+    
+class seriesView(TemplateView):
+    template_name= "series.html"
+    
+class jogosView(TemplateView):
+    template_name= "jogos.html"
+    
+class animesView(TemplateView):
+    template_name= "animes.html"
+    
+class sobreView(TemplateView):
+    template_name= 'sobre.html'
+    
+class contatoView(TemplateView):
+    template_name= 'contato.html'
+    
+class ajudaView(TemplateView):
+    template_name= 'ajuda.html'
