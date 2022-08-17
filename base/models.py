f6ca5caf8b3d69f40c986f72c785ae1fc92663ba
Pedro Lucas <80356903+limapedro2002@users.noreply.github.com>
2022-08-17 16:04:24 -0300

    user

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(Base):
+    email = models.EmailField("E-mail", max_length=100)
+    password = models.CharField("Senha", max_length=20)
+    
+    def __str__(self):
+        return self.email
